9aa4c77bd2f3ace0ac29b70a8cf1f6bf0c7e05fc
YaphetS <qwer_lala@mail.ru>
2023-01-21 19:39:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## React Whatsapp Clone
+
+made with React & Styled components
+
+**[VIEW DEMO](https://react-chat-thomas.netlify.com/)**
+
+## Screenshots
+
+![HOME](/public/screenshot1.png)
---
+++ src/data/chats.js
@@ -0,0 +1,66 @@
+const chats = [
+  {
+    id: 64138,
+    title: "Pete 😎",
+    preview: "Hey!",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/gauchomatt/128.jpg"
+  },
+  {
+    id: 10235,
+    title: "Wilson Kuhn",
+    preview: "Nemo recusandae molestias qui recusandae enim in natus.",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/brunodesign1206/128.jpg"
+  },
+  {
+    id: 64987,
+    title: "Yazmin Kuhlman 🚀 🔥",
+    preview: "Quia fugiat sint inventore.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/romanbulah/128.jpg"
+  },
+  {
+    id: 43915,
+    title: "Rita Leffler",
+    preview: "Sapiente et sit est modi ut debitis.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/jm_denis/128.jpg"
+  },
+  {
+    id: 64732,
+    title: "Catalina Krajcik😂😂",
+    preview: "Eveniet praesentium qui est quia ea labore est.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/muridrahhal/128.jpg"
+  },
+  {
+    id: 82412,
+    title: "Dr. Patience Smitham",
+    preview: "Natus aspernatur voluptatem itaque.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/cocolero/128.jpg"
+  },
+  {
+    id: 17619,
+    title: "Joshua Ledner",
+    preview: "Vel distinctio deserunt officia voluptas quam sit.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/polarity/128.jpg"
+  },
+  {
+    id: 50887,
+    title: "Patrick Miller😀",
+    preview: "Dolorem ut voluptas officia nisi consequatur ab.",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/bartoszdawydzik/128.jpg"
+  },
+  {
+    id: 83093,
+    title: "Kenny Flatley",
+    preview: "Ea quaerat et repellendus in atque.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/josemarques/128.jpg"
+  },
+  {
+    id: 45216,
+    title: "Rosalee Lebsack",
+    preview: "Cumque sequi iusto reiciendis accusantium ad velit iste et eos.",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/areus/128.jpg"
+  }
+];
+
+export { chats };
